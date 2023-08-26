e0fd9dbdd8ec4fedcebca53a7708d6f038d3578d
Jo Chevy <chevyjd2007@gmail.com>
2023-08-26 12:43:39 -0400

    +fixed validParenthesis

--- src/ValidParenthesis.java
+++ src/ValidParenthesis.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class ValidParenthesis {
 
     public static void main(String[] args) {
-        String example = ")";
+        String example = "";
 
         Boolean answer = valid(example);
 
@@ -12,14 +14,11 @@ public class ValidParenthesis {
 
     public static boolean valid(String s) {
         Stack<Character> model = new Stack<>();
-        // Return false if the String s is empty
-        if (s != "") {
             // Iterating through the entire length of s to check each of its characters
             for (int i = 0; i < s.length(); i++) {
                 // If we encounter an open character, then we push it to the stack
                 if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                     model.push(s.charAt(i));
-
                     /* If we encounter an open statement in s while model is not empty and
                      * the last element in the stack is an open statement of the same type,
                      * then we pop the latest character from the stack.
@@ -31,11 +30,12 @@ public class ValidParenthesis {
                 } else if (s.charAt(i) == '}' && model.size() > 0 && model.peek() == '{') {
                     model.pop();
                 }
+                // If we don't encounter any of the open brackets, we just return false
+                else {
+                    return false;
+                }
             }
-        } else {
-            return false;
-        }
-        // If s is not empty, then we return a boolean determining if model is empty or not.
+        // We return a boolean dictating if whether or not the stack model is empty.
         return model.isEmpty();
     }
 }
