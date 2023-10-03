9ab89be9fe02e6b30a1c3e8acbda704465a5e77e
Chevyjd2007 <112202840+Chevyjd2007@users.noreply.github.com>
2023-10-02 20:32:06 -0400

    +number of good pairs

---
+++ src/NumberOfGoodPairs.java
@@ -0,0 +1,16 @@
+public class NumberOfGoodPairs {
+
+    public int numIdenticalPairs(int[] nums) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ src/ha.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ha {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(":");
+        int m = sc.nextInt();
+
+        System.out.println(m);
+    }
+}
