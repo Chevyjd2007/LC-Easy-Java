82196935fddf6ce43c023af4e47cdb7121f4477c
Chevyjd2007 <112202840+Chevyjd2007@users.noreply.github.com>
2023-12-19 19:30:42 -0500

    +more efficient solution

--- src/BuyTwoChocolates.java
+++ src/BuyTwoChocolates.java
@@ -16,4 +16,27 @@ public class BuyTwoChocolates {
         return money;
 
     }
+
+    // linear complexity
+    public int buyChoco2(int[] prices, int money) {
+
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int c : prices) {
+            if (c < a) {
+                b = a;
+                a = c;
+            } else if (c < b) {
+                b = c;
+            }
+        }
+
+        int answer = money - (a + b);
+
+        if (answer < 0)
+            return money;
+
+        return answer;
+    }
 }
